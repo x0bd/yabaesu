95d0bb799416c6b955b605bd79fbbdd6a3aecf61
xoboid <112933783+x0bd@users.noreply.github.com>
2025-03-12 21:25:27 +0200

    not gonna do this

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,10 @@
-console.log("Hello via Bun!");
-console.log("Competency Porn");
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleListening = () => {
+	console.log(`Server running on http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
